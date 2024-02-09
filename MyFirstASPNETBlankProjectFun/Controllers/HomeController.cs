302f1454442b1b39a5b873bb537c31e798b9bf31
jackcarpenter <jackcarpenter@JACKCARPENT0FE0>
2024-02-09 12:06:44 -0700

    Add project files.

---
+++ MyFirstASPNETBlankProjectFun/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// This is my HomeController page
+namespace MyFirstASPNETBlankProjectFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Package_Calculations()
+        {
+            return View();
+        }
+    }
+}
